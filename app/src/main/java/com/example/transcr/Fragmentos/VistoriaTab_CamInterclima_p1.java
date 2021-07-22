421ede002d299d224e055603c3eb658e9f6b6ca8
henri <r!que4568>
2021-07-22 14:15:49 -0300

    Start Commit!

--- app/src/main/java/com/example/transcr/Fragmentos/VistoriaTab_CamInterclima_p1.java
+++ app/src/main/java/com/example/transcr/Fragmentos/VistoriaTab_CamInterclima_p1.java
@@ -4,7 +4,6 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +12,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
